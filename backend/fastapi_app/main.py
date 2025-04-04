5db38a62dee2208307688ed7d0cb982aed79fd3a
BhavinMerai <51120063+BhavinMerai@users.noreply.github.com>
2025-04-04 12:59:19 +0000

    baseline modification in main.py

--- backend/fastapi_app/main.py
+++ backend/fastapi_app/main.py
@@ -1,11 +1,14 @@
 # backend/fastapi_app/main.py
 from fastapi import FastAPI
-
+from api.v1.endpoints import execute, status
 # This is the FastAPI app instance that Uvicorn is looking for
-app = FastAPI()
+app = FastAPI(title="Databricks code executor")
 
+# # Define a simple route to test
+# @app.get("/")
+# def read_root():
+#     return {"message": "FastAPI is running!"}
 
-# Define a simple route to test
-@app.get("/")
-def read_root():
-    return {"message": "FastAPI is running!"}
+#Register endpoints
+app.include_router(execute.router, prefix="/v1")
+app.include_router(status.router, prefix="/v1")
